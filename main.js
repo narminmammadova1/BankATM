34ddf99d19324a06d6a3197b676a571c8a91b5fc
narminmammadova1 <nerminytb89@gmail.com>
2023-08-14 21:30:06 +0400

    added new items

--- main.js
+++ main.js
@@ -12,78 +12,135 @@ const cashMessage = document.querySelector("#cashMessage");
 const container=document.querySelector(".container")
 const chk = document.querySelector("#chk");
 const thirdPage=document.querySelector("#thirdPage")
-const customerInfo = [];
+// const customerInfo = [];
 let balance = 200;
 
 btnEnter.
 addEventListener("click", function () {
-    start()
-});
-
-function start(){const value = fullName.value;
-    customerInfo.
-   push(value);
+    event.preventDefault();
+    const value = fullName.value;
+    if(fullName.value==="" || password.value===""){alert("please enter your account and password")
+    }
+else{
+//     customerInfo.
+//    push(value);
 render();
     console.log(customerInfo);
+ 
+}});
 
-}
+
 
 chk.
 addEventListener("change", function () {
     password.type = chk.checked ? "text" : "password";
 });
 
-btnAdd.
-addEventListener("click", function () {
-    add();
-});
+// btnAdd.
+// addEventListener("click", function () {
+//     add();
+// });
 
-btnCash.addEventListener("click", function () {
-    render();
-    cash();
-});
+// btnCash.addEventListener("click", function () {
+//     // render();
+//     cash();
+// });
+
+// function render() {
+//     firstPage.
+// style.display = "none";
+//     secondPage.style.display = "block";
+//     let content = `Dear <span style="color:red;">${fullName.value}</span>, welcome to AyBank APP.<br>Your Current balance ${balance} $.`;
+//     welcome.innerHTML = content;
+//     fullName.value = "";
+// }
+
+// function add() {
+//     if (addInput.value === "") {
+// alert("Please enter an amount.");
+//     } else {
+//         balance += parseFloat(addInput.value);
+//         let content = `Your Current balance ${balance} $.`;
+//         welcome.
+// innerHTML = content;
+//         addInput.value = "";
+//     }
+// }
+
+
+   
+// function cash() {
+// if (addInput.value === "") { 
+// alert("Please enter an amount");
+//     } 
+    
+// else {
+//         if (addInput.value <= balance) {
+//             balance -= parseFloat(addInput.value);
+//             welcome.
+// innerHTML = `Your Current balance ${balance} $.`;
+//             cashMessage.
+// innerHTML = `${addInput.value} $ is ready. Take it. Your current balance is ${balance} $.`;
+//             addInput.value = "";
+//         } else { 
+// alert("Low balance");
+//         }
+//     }
+// }
+// btnExit.addEventListener("click",function(){
+//     thirdPage.style.display="block"
+// })
 
 function render() {
-    firstPage.
-style.display = "none";
+    firstPage.style.display = "none";
     secondPage.style.display = "block";
     let content = `Dear <span style="color:red;">${fullName.value}</span>, welcome to AyBank APP.<br>Your Current balance ${balance} $.`;
     welcome.innerHTML = content;
     fullName.value = "";
 }
 
+
+btnAdd.addEventListener("click", function () {
+    add()
+    // render();
+});
+
+btnCash.addEventListener("click", function () {
+    // render()
+    cash()
+    ;
+});
+
+
+
 function add() {
+    event.preventDefault();
     if (addInput.value === "") {
-alert("Please enter an amount.");
-    } else {
+        alert("Please enter an amount.")}
+         else {
         balance += parseFloat(addInput.value);
         let content = `Your Current balance ${balance} $.`;
-        welcome.
-innerHTML = content;
+        welcome.innerHTML = content;
         addInput.value = "";
     }
 }
 
-
-   
 function cash() {
-if (addInput.value === "") { 
-alert("Please enter an amount");
-    } 
-    
-else {
+    event.preventDefault();
+    if (addInput.value === "") { 
+        alert("Please enter an amount");
+    } else {
         if (addInput.value <= balance) {
             balance -= parseFloat(addInput.value);
-            welcome.
-innerHTML = `Your Current balance ${balance} $.`;
-            cashMessage.
-innerHTML = `${addInput.value} $ is ready. Take it. Your current balance is ${balance} $.`;
-            addInput.value = "";
-        } else { 
-alert("Low balance");
+            welcome.innerHTML = `Your Current balance ${balance} $.`;
+            cashMessage.innerHTML = `${addInput.value} $ is ready. Take it. Your current balance is ${balance} $.`;
+            addInput.value = "" }
+             else { 
+            alert("Low balance");
         }
     }
 }
-btnExit.addEventListener("click",function(){
-    thirdPage.style.display="block"
-})
+
+btnExit.addEventListener("click", function () {
+    firstPage.style.display = "block";
+});
