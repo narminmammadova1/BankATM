fd762fd4e62ea96b43da8b489bea7b480d33891e
narminmammadova1 <nerminytb89@gmail.com>
2023-08-14 20:17:41 +0400

    added new file

---
+++ constructorBANK.js
@@ -0,0 +1,22 @@
+function Bank(fullName,balance){
+
+    this.fullName=fullName
+    this.balance=balance
+    this.addBalance=function(
+
+    ){}
+
+this.cashBalance=function(){}
+this.render=function(){}
+    // console.log(this);
+}
+
+ new Bank()
+
+ const nerminCard=new Bank("Nermin","200")
+ console.log(nerminCard);
+
+
+ const humaycard=new Bank("Humay","400")
+
+ console.log(humaycard);
---
+++ main.js
@@ -0,0 +1,89 @@
+const fullName = document.querySelector("#fullName");
+const password = document.querySelector("#password");
+const btnEnter = document.querySelector("#btnEnter");
+const btnAdd = document.querySelector("#btnAdd");
+const btnCash = document.querySelector("#btnCash");
+const btnExit=document.querySelector("#btnExit")
+const addInput = document.querySelector("#addInput");
+const welcome = document.querySelector(".welcome");
+const firstPage = document.querySelector("#firstPage");
+const secondPage = document.querySelector("#secondPage");
+const cashMessage = document.querySelector("#cashMessage");
+const container=document.querySelector(".container")
+const chk = document.querySelector("#chk");
+const thirdPage=document.querySelector("#thirdPage")
+const customerInfo = [];
+let balance = 200;
+
+btnEnter.
+addEventListener("click", function () {
+    start()
+});
+
+function start(){const value = fullName.value;
+    customerInfo.
+   push(value);
+render();
+    console.log(customerInfo);
+
+}
+
+chk.
+addEventListener("change", function () {
+    password.type = chk.checked ? "text" : "password";
+});
+
+btnAdd.
+addEventListener("click", function () {
+    add();
+});
+
+btnCash.addEventListener("click", function () {
+    render();
+    cash();
+});
+
+function render() {
+    firstPage.
+style.display = "none";
+    secondPage.style.display = "block";
+    let content = `Dear <span style="color:red;">${fullName.value}</span>, welcome to AyBank APP.<br>Your Current balance ${balance} $.`;
+    welcome.innerHTML = content;
+    fullName.value = "";
+}
+
+function add() {
+    if (addInput.value === "") {
+alert("Please enter an amount.");
+    } else {
+        balance += parseFloat(addInput.value);
+        let content = `Your Current balance ${balance} $.`;
+        welcome.
+innerHTML = content;
+        addInput.value = "";
+    }
+}
+
+
+   
+function cash() {
+if (addInput.value === "") { 
+alert("Please enter an amount");
+    } 
+    
+else {
+        if (addInput.value <= balance) {
+            balance -= parseFloat(addInput.value);
+            welcome.
+innerHTML = `Your Current balance ${balance} $.`;
+            cashMessage.
+innerHTML = `${addInput.value} $ is ready. Take it. Your current balance is ${balance} $.`;
+            addInput.value = "";
+        } else { 
+alert("Low balance");
+        }
+    }
+}
+btnExit.addEventListener("click",function(){
+    thirdPage.style.display="block"
+})
