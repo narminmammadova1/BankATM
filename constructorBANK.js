9e3064bfb1c85bf7ebf49e738fd3e6f36901c412
narminmammadova1 <nerminytb89@gmail.com>
2023-08-14 22:55:58 +0400

    modified

--- constructorBANK.js
+++ constructorBANK.js
@@ -2,21 +2,17 @@ function Bank(fullName,balance){
 
     this.fullName=fullName
     this.balance=balance
-    this.addBalance=function(
+    this.add=function(
+    ){
+        
+    }
 
-    ){}
-
-this.cashBalance=function(){}
+this.cash=function(){
+    }
 this.render=function(){}
-    // console.log(this);
 }
 
  new Bank()
 
- const nerminCard=new Bank("Nermin","200")
- console.log(nerminCard);
-
-
- const humaycard=new Bank("Humay","400")
-
- console.log(humaycard);
+ const nermincard=new Bank("nermin","500")
+ console.log(nermincard);
--- main.js
+++ main.js
@@ -8,11 +8,7 @@ const addInput = document.querySelector("#addInput");
 const welcome = document.querySelector(".welcome");
 const firstPage = document.querySelector("#firstPage");
 const secondPage = document.querySelector("#secondPage");
-const cashMessage = document.querySelector("#cashMessage");
-const container=document.querySelector(".container")
-const chk = document.querySelector("#chk");
-const thirdPage=document.querySelector("#thirdPage")
-// const customerInfo = [];
+const customerInfo = [];
 let balance = 200;
 
 btnEnter.
@@ -22,74 +18,19 @@ addEventListener("click", function () {
     if(fullName.value==="" || password.value===""){alert("please enter your account and password")
     }
 else{
-//     customerInfo.
-//    push(value);
+    customerInfo.
+   push(value);
 render();
     console.log(customerInfo);
  
 }});
-
-
 
 chk.
 addEventListener("change", function () {
     password.type = chk.checked ? "text" : "password";
 });
 
-// btnAdd.
-// addEventListener("click", function () {
-//     add();
-// });
 
-// btnCash.addEventListener("click", function () {
-//     // render();
-//     cash();
-// });
-
-// function render() {
-//     firstPage.
-// style.display = "none";
-//     secondPage.style.display = "block";
-//     let content = `Dear <span style="color:red;">${fullName.value}</span>, welcome to AyBank APP.<br>Your Current balance ${balance} $.`;
-//     welcome.innerHTML = content;
-//     fullName.value = "";
-// }
-
-// function add() {
-//     if (addInput.value === "") {
-// alert("Please enter an amount.");
-//     } else {
-//         balance += parseFloat(addInput.value);
-//         let content = `Your Current balance ${balance} $.`;
-//         welcome.
-// innerHTML = content;
-//         addInput.value = "";
-//     }
-// }
-
-
-   
-// function cash() {
-// if (addInput.value === "") { 
-// alert("Please enter an amount");
-//     } 
-    
-// else {
-//         if (addInput.value <= balance) {
-//             balance -= parseFloat(addInput.value);
-//             welcome.
-// innerHTML = `Your Current balance ${balance} $.`;
-//             cashMessage.
-// innerHTML = `${addInput.value} $ is ready. Take it. Your current balance is ${balance} $.`;
-//             addInput.value = "";
-//         } else { 
-// alert("Low balance");
-//         }
-//     }
-// }
-// btnExit.addEventListener("click",function(){
-//     thirdPage.style.display="block"
-// })
 
 function render() {
     firstPage.style.display = "none";
@@ -102,11 +43,9 @@ function render() {
 
 btnAdd.addEventListener("click", function () {
     add()
-    // render();
 });
 
 btnCash.addEventListener("click", function () {
-    // render()
     cash()
     ;
 });
@@ -119,7 +58,7 @@ function add() {
         alert("Please enter an amount.")}
          else {
         balance += parseFloat(addInput.value);
-        let content = `Your Current balance ${balance} $.`;
+        let content = `Processing successful. Your Current balance ${balance} $.`;
         welcome.innerHTML = content;
         addInput.value = "";
     }
@@ -132,8 +71,7 @@ function cash() {
     } else {
         if (addInput.value <= balance) {
             balance -= parseFloat(addInput.value);
-            welcome.innerHTML = `Your Current balance ${balance} $.`;
-            cashMessage.innerHTML = `${addInput.value} $ is ready. Take it. Your current balance is ${balance} $.`;
+            welcome.innerHTML = `Processing successful.Your Current balance ${balance} $.`;
             addInput.value = "" }
              else { 
             alert("Low balance");
